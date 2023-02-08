118fef581e70687b0f200a968048d1c18c39b866
izmeera2000 <izmeera2000@gmail.com>
2023-02-08 14:33:22 +0800

    test new webserver

--- esp8266/oximeter_server5/oximeter_server5.ino
+++ esp8266/oximeter_server5/oximeter_server5.ino
@@ -1,224 +1,166 @@
-//HEART RATE AND OXYGEN SATURATION CLOUD STORAGE BASED FOR REMOTE PATIENT
-//blynk akaun berbayar   uname : 29221097@adtectaiping.edu.my
-//                       pwd :adtectaiping2022
-
-// #define BLYNK_TEMPLATE_ID "TMPLHQPlWoZS"
-// #define BLYNK_DEVICE_NAME "REMOTE PATIENT MONITORING SYSTEM"
-// #define BLYNK_AUTH_TOKEN "OOHrk_fEv6ftdQW4Y6mdqFDUSMf4QEP6"
-
-#define REPORTING_PERIOD_MS 1000
-
-#include <Adafruit_GFX.h>  //OLED libraries
-#include <Adafruit_SSD1306.h>
+//MAX30100 ESP8266 WebServer
+#include <ESP8266WebServer.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <ESP8266WiFi.h>  // wifi
-// #include <Blynk.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-// #include <BlynkSimpleEsp8266.h>
 
-// PulseOximeter is the higher level interface to the sensor
-// it offers:
-//  * heart rate calculation
-//  * SpO2 (oxidation level) calculation
+
+#define REPORTING_PERIOD_MS     1000
+
+float BPM, SpO2;
+
+/*Put your SSID & Password*/
+const char* ssid = "afa2020_2.4Ghz@unifi";  // Enter SSID here
+const char* password = "vae585910";  //Enter Password here
+
 PulseOximeter pox;
-
 uint32_t tsLastReport = 0;
 
-//wifi
-const char *ssid = "afa2020_2.4Ghz@unifi";                           // afa2020_2.4Ghz@unifi , KOMPUTER, vivo1713
-const char *pass = "vae585910";                                      // vae585910 , NIL, vae585910
-String serverName = "http://192.168.1.7/oximeterserver/insert.php";  //check sebelum upload
-String apiKeyValue = "oxytest";
-String sensorname = "oxy1";
-
-
-
-
-//gunakan ip untuk local server
-
-
-
-//TinyGPSPlus gps;
-//SoftwareSerial SerialGPS(4, 5); //GPIO 4 & 5
-// #define BLYNK_PRINT Serial
-
-float Latitude, Longitude;
-int bpm, spo;
-
-//OLED
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 32  // OLED display height, in pixels 32
-#define OLED_RESET -1     // Reset pin # (or -1 if sharing Arduino reset pin)
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  //Declaring the display name (display)
-
-//LED
-#define LED_pin7 D7  // tentukan nama device pada pin
-#define LED_pin6 D6
-#define LED_pin5 D5
-#define LED_pin4 D4
-#define LED_pin3 D3
-#define LED_pin8 D8
-
-
-
+ESP8266WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  //  SerialGPS.begin(9600);
-  // Blynk.begin(auth, ssid, pass);
+  pinMode(16, OUTPUT);
+  delay(100);
 
-  //Wifi connection
-  WiFi.begin(ssid, pass);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+  
+  //check wi-fi is connected to wi-fi network
   while (WiFi.status() != WL_CONNECTED) {
-    delay(200);
-    Serial.print("..");
+    delay(1000);
+    Serial.print(".");
   }
-  Serial.println();
-  Serial.println("Congrats... NodeMCU is connected!");
-  Serial.println(WiFi.localIP());
-  // dht.begin();
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
 
-
-  //OLED
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //Start the OLED display
-  display.display();
-  delay(3000);
-  //asalnya
+  server.begin();
+  Serial.println("HTTP server started");
+  
   Serial.print("Initializing pulse oximeter..");
 
-  // Initialize the PulseOximeter instance
-  // Failures are generally due to an improper I2C wiring, missing power supply
-  // or wrong target chip
   if (!pox.begin()) {
     Serial.println("FAILED");
-    for (;;)
-      ;
-  } else
+    for (;;);
+  } else {
     Serial.println("SUCCESS");
-  // {
-  //   // timer.setInterval(3000L, timer1);
-  // }
-  //LED
-  pinMode(LED_pin7, OUTPUT);  // declare pin samada input @ output
-  pinMode(LED_pin6, OUTPUT);
-  pinMode(LED_pin5, OUTPUT);
-  pinMode(LED_pin4, OUTPUT);
-  pinMode(LED_pin3, OUTPUT);
-  pinMode(LED_pin8, OUTPUT);
+    
+  }
 }
-
 void loop() {
-  // Make sure to call update as fast as possible
+  server.handleClient();
   pox.update();
-  // timer.run();
-  // Blynk.run();
-
-
-  // Asynchronously dump heart rate and oxidation levels to the serial
-  // For both, a value of 0 means "invalid"
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
 
-    tsLastReport = millis();
+    BPM = pox.getHeartRate();
+    SpO2 = pox.getSpO2();
 
-    display.clearDisplay();  //Clear the display
-    display.setTextSize(1);  //Near it display the average BPM you can display the BPM if you want
-    display.setTextColor(WHITE);
-    display.setCursor(30, 0);
-    display.println("BPM");
-    display.setCursor(30, 8);
-    display.println(bpm);
-    display.setCursor(90, 0);  //80,0
-    display.println("SpO2");
-    display.setCursor(90, 8);  // 82,18
-    display.println(spo);
-    display.setCursor(0, 16);  //80,0
-    display.println("Lat   :");
-    display.setCursor(0, 24);  //80,0
-    display.println("Long  :");
-    display.display();
+    Serial.print("BPM: ");
+    Serial.println(BPM);
 
-    Serial.print("Heart rate:");
-    Serial.print(pox.getHeartRate());
-    Serial.print("bpm    SpO2:");  //"bpm / SpO2:"
-    Serial.print(pox.getSpO2());
+    Serial.print("SpO2: ");
+    Serial.print(SpO2);
     Serial.println("%");
 
-
-    if (pox.getHeartRate() < 60) {
-      digitalWrite(LED_pin5, HIGH);  //LED MERAH on
-      digitalWrite(LED_pin7, LOW);   //LED HIJAU on
-      digitalWrite(LED_pin6, LOW);   //LED KUNING on
-    }
-    if (pox.getHeartRate() > 100) {
-      digitalWrite(LED_pin6, HIGH);  //LED KUNING on
-      digitalWrite(LED_pin5, LOW);   //LED MERAH on
-      digitalWrite(LED_pin7, LOW);   //LED HIJAU on
-    }
-    if (pox.getHeartRate() > 60 && pox.getHeartRate() < 100) {
-      digitalWrite(LED_pin7, HIGH);  //LED HIJAU on
-      digitalWrite(LED_pin5, LOW);   //LED MERAH on
-      digitalWrite(LED_pin6, LOW);   //LED KUNING on
-    }
-
-
-    if (pox.getSpO2() > 94) {
-      digitalWrite(LED_pin3, LOW);   //LED MERAH oFF
-      digitalWrite(LED_pin4, HIGH);  //LED HIJAU ON
-      digitalWrite(LED_pin8, LOW);   //LED KUNING oFF
-    }
-
-    if (pox.getSpO2() > 89 && pox.getSpO2() < 95) {
-      digitalWrite(LED_pin3, LOW);   //LED MERAH oFF
-      digitalWrite(LED_pin4, LOW);   //LED HIJAU oFF
-      digitalWrite(LED_pin8, HIGH);  //LED KUNING ON
-    }
-
-    if (pox.getSpO2() < 90) {
-      digitalWrite(LED_pin3, HIGH);  //LED MERAH ON
-      digitalWrite(LED_pin4, LOW);   //LED HIJAU oFF
-      digitalWrite(LED_pin8, LOW);   //LED KUNING oFF
-    }
-
-    {
-      bpm = pox.getHeartRate();
-      spo = pox.getSpO2();
-      // Blynk.virtualWrite(V5, bpm);
-      // Blynk.virtualWrite(V6, spo);
-
-      if (WiFi.status() == WL_CONNECTED) {
-        WiFiClient client;
-        HTTPClient http;
-
-        // Your Domain name with URL path or IP address with path
-        http.begin(client, serverName);
-
-        // Specify content-type header
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-        String httpRequestData = "api_key=" + apiKeyValue + "&bpm=" + String(pox.getHeartRate()) + "&o2=" + String(pox.getSpO2()) + "";
-        Serial.print("httpRequestData: ");
-        Serial.println(httpRequestData);
-
-        int httpResponseCode = http.POST(httpRequestData);
-
-
-        if (httpResponseCode > 0) {
-          Serial.print("HTTP Response code: ");
-          Serial.println(httpResponseCode);
-        } else {
-          Serial.print("Error code: ");
-          Serial.println(httpResponseCode);
-        }
-        // Free resources
-        http.end();
-      } else {
-        Serial.println("WiFi Disconnected");
-      }
-    }
+    Serial.println("*********************************");
+    Serial.println();
+    tsLastReport = millis();
   }
+
+
+}
+
+void handle_OnConnect() {
+
+  server.send(200, "text/html", SendHTML(BPM, SpO2));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float BPM, float SpO2) {
+  String ptr = "<!DOCTYPE html>";
+  ptr += "<html>";
+  ptr += "<head>";
+  ptr += "<title>ESP8266 WebServer</title>";
+  ptr += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+  ptr += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
+  ptr += "<link rel='stylesheet' type='text/css' href='styles.css'>";
+  ptr += "<style>";
+  ptr += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, sans-serif box-sizing: border-box;}";
+  ptr += "#page { margin: 20px; background-color: #fff;}";
+  ptr += ".container { height: inherit; padding-bottom: 20px;}";
+  ptr += ".header { padding: 20px;}";
+  ptr += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
+  ptr += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
+  ptr += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
+  ptr += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
+  ptr += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
+  ptr += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
+  ptr += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
+  ptr += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
+  ptr += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
+  ptr += ".units { font-size: 1.2rem;}";
+  ptr += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
+  ptr += "</style>";
+
+  //Ajax Code Start
+  ptr += "<script>\n";
+  ptr += "setInterval(loadDoc,1000);\n";
+  ptr += "function loadDoc() {\n";
+  ptr += "var xhttp = new XMLHttpRequest();\n";
+  ptr += "xhttp.onreadystatechange = function() {\n";
+  ptr += "if (this.readyState == 4 && this.status == 200) {\n";
+  ptr += "document.body.innerHTML =this.responseText}\n";
+  ptr += "};\n";
+  ptr += "xhttp.open(\"GET\", \"/\", true);\n";
+  ptr += "xhttp.send();\n";
+  ptr += "}\n";
+  ptr += "</script>\n";
+  //Ajax Code END
+
+  ptr += "</head>";
+  ptr += "<body>";
+  ptr += "<div id='page'>";
+  ptr += "<div class='header'>";
+  ptr += "<h1>MAX30100 ESP8266 WebServer</h1>";
+  ptr += "<h3><a href='https://iotprojectsideas.com'>https://theiotprojects.com</a></h3>";
+  ptr += "</div>";
+  ptr += "<div id='content' align='center'>";
+  ptr += "<div class='box-full' align='left'>";
+  ptr += "<h2>Sensor Readings</h2>";
+  ptr += "<div class='sensors-container'>";
+
+  //For Heart Rate
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
+  ptr += "<span class='sensor-labels'> Heart Rate </span>";
+  ptr += (int)BPM;
+  ptr += "<sup class='units'>BPM</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+  //For Sp02
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-burn' style='color:#f7347a'></i>";
+  ptr += "<span class='sensor-labels'> Sp02 </span>";
+  ptr += (int)SpO2;
+  ptr += "<sup class='units'>%</sup>";
+  ptr += "</p>";
+
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</body>";
+  ptr += "</html>";
+  return ptr;
 }
