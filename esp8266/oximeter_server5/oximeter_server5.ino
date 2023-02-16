584d90a67fde5bdeac487953cef2ca72e6a926ca
izmeera2000 <izmeera2000@gmail.com>
2023-02-16 22:55:43 +0800

    test5

--- esp8266/oximeter_server5/oximeter_server5.ino
+++ esp8266/oximeter_server5/oximeter_server5.ino
@@ -129,7 +129,7 @@ void loop()
         http.begin(client, serverName);
         http.addHeader("Content-Type", "application/x-www-form-urlencoded");
         //  http.addHeader("Content-Type", "text/plain");
-        String httpRequestData = "api_key=" + apiKeyValue + "&bpm=99" + "&o2=99" + "sensorname=" + sensorname;
+        String httpRequestData = "api_key=" + apiKeyValue + "&bpm=99" + "&o2=99&" + "sensorname=" + sensorname;
         // asdasdas
         // String httpRequestData = "api_key=" + apiKeyValue + "&bpm=" + String(pox.getHeartRate()) + "&o2=" + String(pox.getSpO2()) + "sensorname=" + sensorname;
         Serial.print("httpRequestData: ");
