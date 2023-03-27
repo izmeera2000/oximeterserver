d0bcb6e09038d155bdeaca4593d9bf9def5bd088
izmeera2000 <izmeera2000@gmail.com>
2023-03-28 01:33:22 +0800

    test move last erport

--- esp8266/oximeter_server6/oximeter_server6.ino
+++ esp8266/oximeter_server6/oximeter_server6.ino
@@ -1,4 +1,4 @@
-#include <Adafruit_GFX.h>  //OLED libraries
+#include <Adafruit_GFX.h> //OLED libraries
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
@@ -18,19 +18,19 @@ TaskHandle_t Task2;
 
 uint32_t tsLastReport = 0;
 
-const char *ssid = "afa2020_2.4Ghz@unifi";                           // afa2020_2.4Ghz@unifi , KOMPUTER, vivo1713
-const char *pass = "vae585910";                                      // vae585910 , NIL, vae585910
-String serverName = "http://192.168.1.9/oximeterserver/insert.php";  // check sebelum upload
+const char *ssid = "afa2020_2.4Ghz@unifi";                          // afa2020_2.4Ghz@unifi , KOMPUTER, vivo1713
+const char *pass = "vae585910";                                     // vae585910 , NIL, vae585910
+String serverName = "http://192.168.1.9/oximeterserver/insert.php"; // check sebelum upload
 String apiKeyValue = "oxytest";
 String sensorname = "oxy1";
 float BPM, SpO2;
 
 // OLED
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 32  // OLED display height, in pixels 32
-#define OLED_RESET -1     // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels 32
+#define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  // Declaring the display name (display)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Declaring the display name (display)
 
 // LED
 // #define LED_pin4 4  // tentukan nama device pada pin
@@ -40,8 +40,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  // De
 // #define LED_pin18 18
 // #define LED_pin5 5
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(16, OUTPUT);
   delay(100);
@@ -52,7 +52,8 @@ void setup() {
   WiFi.begin(ssid, pass);
   Serial.println("Connecting");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
@@ -65,28 +66,28 @@ void setup() {
   // or wrong target chip
 
   // OLED
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Start the OLED display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Start the OLED display
   display.display();
   delay(3000);
 
   xTaskCreatePinnedToCore(
-    Task1code, /* Task function. */
-    "Task1",   /* name of task. */
-    10000,     /* Stack size of task */
-    NULL,      /* parameter of the task */
-    1,         /* priority of the task */
-    &Task1,    /* Task handle to keep track of created task */
-    0);        /* pin task to core 0 */
+      Task1code, /* Task function. */
+      "Task1",   /* name of task. */
+      10000,     /* Stack size of task */
+      NULL,      /* parameter of the task */
+      1,         /* priority of the task */
+      &Task1,    /* Task handle to keep track of created task */
+      0);        /* pin task to core 0 */
   delay(500);
 
-    xTaskCreatePinnedToCore(
-    Task2code, /* Task function. */
-    "Task2",   /* name of task. */
-    10000,     /* Stack size of task */
-    NULL,      /* parameter of the task */
-    1,         /* priority of the task */
-    &Task2,    /* Task handle to keep track of created task */
-    1);        /* pin task to core 0 */
+  xTaskCreatePinnedToCore(
+      Task2code, /* Task function. */
+      "Task2",   /* name of task. */
+      10000,     /* Stack size of task */
+      NULL,      /* parameter of the task */
+      1,         /* priority of the task */
+      &Task2,    /* Task handle to keep track of created task */
+      1);        /* pin task to core 0 */
   delay(500);
 
   // pinMode(LED_pin4, OUTPUT);  // declare pin samada input @ output
@@ -97,133 +98,137 @@ void setup() {
   // pinMode(LED_pin5, OUTPUT);
 
   Serial.print("Initializing pulse oximeter..");
-  if (!pox.begin()) {
+  if (!pox.begin())
+  {
     Serial.println("FAILED");
     for (;;)
       ;
-  } else {
+  }
+  else
+  {
     Serial.println("SUCCESS");
   }
 }
 
-void Task1code(void *pvParameters) {
+void Task1code(void *pvParameters)
+{
 
-  for (;;) {
-    if (WiFi.status() == WL_CONNECTED) {
-    
-        Serial.print("Task1 running on core ");
-        Serial.println(xPortGetCoreID());
-        WiFiClient client;
-        HTTPClient http;
-        http.begin(client, serverName);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  for (;;)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
 
-        String httpRequestData = "api_key=" + apiKeyValue + "&bpm=" + String(BPM) + "&o2=" + String(SpO2) + "&sensorname=" + sensorname;
-        Serial.print("httpRequestData: ");
-        Serial.println(httpRequestData);
-        int httpResponseCode = http.POST(httpRequestData);
-        String payload = http.getString();
-        Serial.println("PAYLOAD");
-        Serial.println(payload);
-        if (httpResponseCode > 0) {
-          Serial.print("HTTP Response code: ");
-          Serial.println(httpResponseCode);
-        } else {
-          Serial.print("Error code: ");
-          Serial.println(httpResponseCode);
-        }
-        http.end();
-    
+      Serial.print("Task1 running on core ");
+      Serial.println(xPortGetCoreID());
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+      String httpRequestData = "api_key=" + apiKeyValue + "&bpm=" + String(BPM) + "&o2=" + String(SpO2) + "&sensorname=" + sensorname;
+      Serial.print("httpRequestData: ");
+      Serial.println(httpRequestData);
+      int httpResponseCode = http.POST(httpRequestData);
+      String payload = http.getString();
+      Serial.println("PAYLOAD");
+      Serial.println(payload);
+      if (httpResponseCode > 0)
+      {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+      }
+      else
+      {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      http.end();
     }
     delay(1000);
   }
 }
 
-void Task2code(void *pvParameters) {
+void Task2code(void *pvParameters)
+{
 
-  for (;;) {
-  // Make sure to call update as fast as possible
-  pox.update();
+  for (;;)
+  {
+    // Make sure to call update as fast as possible
+    pox.update();
 
-  // Asynchronously dump heart rate and oxidation levels to the serial
-  // For both, a value of 0 means "invalid"
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-    Serial.print("loop() running on core ");
-    Serial.println(xPortGetCoreID());
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+    {
+      Serial.print("loop() running on core ");
+      Serial.println(xPortGetCoreID());
 
-    BPM = pox.getHeartRate();
-    SpO2 = pox.getSpO2();
+      BPM = pox.getHeartRate();
+      SpO2 = pox.getSpO2();
 
-    display.clearDisplay();  // Clear the display
-    display.setTextSize(1);  // Near it display the average BPM you can display the BPM if you want
-    display.setTextColor(WHITE);
-    display.setCursor(30, 0);
-    display.println("BPM");
-    display.setCursor(30, 8);
-    display.println(BPM);
-    display.setCursor(90, 0);  // 80,0
-    display.println("SpO2");
-    display.setCursor(90, 8);  // 82,18
-    display.println(SpO2);
-    display.display();
+      display.clearDisplay(); // Clear the display
+      display.setTextSize(1); // Near it display the average BPM you can display the BPM if you want
+      display.setTextColor(WHITE);
+      display.setCursor(30, 0);
+      display.println("BPM");
+      display.setCursor(30, 8);
+      display.println(BPM);
+      display.setCursor(90, 0); // 80,0
+      display.println("SpO2");
+      display.setCursor(90, 8); // 82,18
+      display.println(SpO2);
+      display.display();
 
-    Serial.print("BPM: ");
-    Serial.println(BPM);
+      Serial.print("BPM: ");
+      Serial.println(BPM);
 
-    Serial.print("SpO2: ");
-    Serial.print(SpO2);
-    Serial.println("%");
+      Serial.print("SpO2: ");
+      Serial.print(SpO2);
+      Serial.println("%");
 
-    Serial.println("*********************************");
-    Serial.println();
+      Serial.println("*********************************");
+      Serial.println();
 
+      // if (BPM < 60) {
+      //   digitalWrite(LED_pin4, HIGH);  // LED MERAH on
+      //   digitalWrite(LED_pin2, LOW);   // LED KUNING off
+      //   digitalWrite(LED_pin15, LOW);  // LED HIJAU off
+      // }
+      // if (BPM > 100) {
+      //   digitalWrite(LED_pin4, LOW);   // LED MERAH off
+      //   digitalWrite(LED_pin2, HIGH);  // LED KUNING on
+      //   digitalWrite(LED_pin15, LOW);  // LED HIJAU off
+      // }
+      // if (BPM > 60 && BPM < 100) {
+      //   digitalWrite(LED_pin4, LOW);    // LED MERAH off
+      //   digitalWrite(LED_pin2, LOW);    // LED KUNING off
+      //   digitalWrite(LED_pin15, HIGH);  // LED HIJAU on
+      // }
 
+      // if (SpO2 > 94) {
+      //   digitalWrite(LED_pin19, LOW);  // LED MERAH oFF
+      //   digitalWrite(LED_pin18, LOW);  // LED KUNING oFF
+      //   digitalWrite(LED_pin5, HIGH);  // LED HIJAU ON
+      // }
 
-    // if (BPM < 60) {
-    //   digitalWrite(LED_pin4, HIGH);  // LED MERAH on
-    //   digitalWrite(LED_pin2, LOW);   // LED KUNING off
-    //   digitalWrite(LED_pin15, LOW);  // LED HIJAU off
-    // }
-    // if (BPM > 100) {
-    //   digitalWrite(LED_pin4, LOW);   // LED MERAH off
-    //   digitalWrite(LED_pin2, HIGH);  // LED KUNING on
-    //   digitalWrite(LED_pin15, LOW);  // LED HIJAU off
-    // }
-    // if (BPM > 60 && BPM < 100) {
-    //   digitalWrite(LED_pin4, LOW);    // LED MERAH off
-    //   digitalWrite(LED_pin2, LOW);    // LED KUNING off
-    //   digitalWrite(LED_pin15, HIGH);  // LED HIJAU on
-    // }
+      // if (SpO2 > 89 && SpO2 < 95) {
+      //   digitalWrite(LED_pin19, LOW);   // LED MERAH oFF
+      //   digitalWrite(LED_pin18, HIGH);  // LED KUNING ON
+      //   digitalWrite(LED_pin5, LOW);    // LED HIJAU oFF
+      // }
 
-    // if (SpO2 > 94) {
-    //   digitalWrite(LED_pin19, LOW);  // LED MERAH oFF
-    //   digitalWrite(LED_pin18, LOW);  // LED KUNING oFF
-    //   digitalWrite(LED_pin5, HIGH);  // LED HIJAU ON
-    // }
+      // if (SpO2 < 90){
 
-    // if (SpO2 > 89 && SpO2 < 95) {
-    //   digitalWrite(LED_pin19, LOW);   // LED MERAH oFF
-    //   digitalWrite(LED_pin18, HIGH);  // LED KUNING ON
-    //   digitalWrite(LED_pin5, LOW);    // LED HIJAU oFF
-    // }
-
-    // if (SpO2 < 90){
-
-    //   digitalWrite(LED_pin19, HIGH);  // LED MERAH ON
-    // digitalWrite(LED_pin18, LOW);     // LED KUNING oFF
-    // digitalWrite(LED_pin5, LOW);      // LED HIJAU oFF
-    // }
+      //   digitalWrite(LED_pin19, HIGH);  // LED MERAH ON
+      // digitalWrite(LED_pin18, LOW);     // LED KUNING oFF
+      // digitalWrite(LED_pin5, LOW);      // LED HIJAU oFF
+      // }
 
       tsLastReport = millis();
-
-  }
-
-
-
+    }
   }
 }
 
-
-void loop() {
-  
+void loop()
+{
 }
