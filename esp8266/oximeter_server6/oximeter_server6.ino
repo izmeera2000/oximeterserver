ca6b01f045e837c9de49ebc038cde9e3c7c82e9a
izmeera2000 <izmeera2000@gmail.com>
2023-03-28 00:51:00 +0800

    test6

--- esp8266/oximeter_server6/oximeter_server6.ino
+++ esp8266/oximeter_server6/oximeter_server6.ino
@@ -73,7 +73,7 @@ void setup() {
     "Task1",   /* name of task. */
     10000,     /* Stack size of task */
     NULL,      /* parameter of the task */
-    0,         /* priority of the task */
+    1,         /* priority of the task */
     &Task1,    /* Task handle to keep track of created task */
     0);        /* pin task to core 0 */
   delay(500);
