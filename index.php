e23691e2abfe565512095fc866cdc372aa5de178
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-11-01 01:03:44 +0800

    asdas

--- index.php
+++ index.php
@@ -66,21 +66,9 @@ if (($_SESSION["accesslevel"] == 1)) {
   <meta name="author" content="Łukasz Holeczek">
   <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
   <title>BME680 Server</title>
-  <link rel="apple-touch-icon" sizes="57x57" href="assets/favicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="assets/favicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/favicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="assets/favicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="assets/favicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="assets/favicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="assets/favicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="assets/favicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png">
+
+  <link rel="icon" type="image/png"  href="assets/favicon/parameter-removebg-preview.png">
+
   <meta name="theme-color" content="#ffffff">
   <!-- Vendors styles-->
   <link rel="stylesheet" href="vendors/simplebar/css/simplebar.css">
@@ -132,14 +120,14 @@ if (($_SESSION["accesslevel"] == 1)) {
 
         <a class="header-brand d-md-none" href="#">
           <!-- logo -->
-          <img style="height:48px" src="assets/favicon/android-icon-144x144.png" class="img" alt="Oximeter Logo">
+          <img style="height:48px" src="assets/favicon/parameter-removebg-preview.png" class="img" alt="Oximeter Logo">
           <!-- logo -->
         </a>
         <ul class="header-nav d-none d-md-flex">
           <li class="nav-item">
             <a class="nav-link" href="index.php">
               <!-- logo -->
-              <img style="height:48px" src="assets/favicon/android-icon-144x144.png" class="img" alt="Logo">
+              <img style="height:48px" src="assets/favicon/parameter-removebg-preview.png" class="img" alt="Logo">
               <!-- logo -->
               BME680 Server
             </a>
